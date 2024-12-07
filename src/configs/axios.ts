1b33f7f3c3106634d43bfa4a381dafe61c097991
Ray <elninothepea@gmail.com>
2024-12-07 21:36:30 +0700

    fix config

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = import.meta.env.VITE_BACKEND_API_URL;
 
 if (!baseURL) {
-    throw new Error('REACT_APP_API_URL is not set');
+    throw new Error('VITE_BACKEND_API_URL is not set');
 }
 
 const api = axios.create({
